45c9efa06146aa8b4c24806c6742c1b17afb7ab8
SimbaDima <37153509+SimbaDima@users.noreply.github.com>
2018-03-27 19:57:04 +0300

    Update quadratic_equation.py

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -4,7 +4,7 @@ from math import sqrt
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
-        raise Exception("discriminant < 0")
+        raise Exception("discriminant < 0, roots will not be real")
 
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
